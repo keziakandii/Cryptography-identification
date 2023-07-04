43c4e8b88dae1609d9d15191ec1eb9753556b8b5
Kezia <76508249+keziakandii@users.noreply.github.com>
2023-07-04 00:12:48 -0400

    Update DNN.py to english

--- DNN.py
+++ DNN.py
@@ -9,84 +9,84 @@ from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv(r'feature_runs.csv')
-#将数据Species列由字母转换成数字
+#Convert the Species column from letters to numbers
 #data['Species']=pd.factorize(data.Species)[0]
-#X不要第零列,不要最后一列 第一个:代表全部的行 第二个:代表列
+#X Don't want the zero column, don't want the last column First: represents all rows Second: represents columns
 X = data.iloc[:,:-1].values
-#Y只要特定一列
+#Y only needs a specific column
 Y = data.label.values
 train_x,test_x,train_y,test_y=train_test_split(X,Y)
-#将数据转换成Tensor LongTensor等价于int64
+#Converting data to Tensor LongTensor is equivalent to int64
 train_x = torch.from_numpy(train_x).type(torch.float32)
 train_y = torch.from_numpy(train_y).type(torch.int64)
 test_x = torch.from_numpy(test_x).type(torch.float32)
 test_y = torch.from_numpy(test_y).type(torch.LongTensor)
 
-#数据只有150行故batch也要小一点
+#The data is only 150 lines, so the batch is also a little smaller
 batch = 5
 no_of_batches = len(data)//batch
 epochs = 200
 
-#TensorDataset()可以对tensor进行打包即合并
+#TensorDataset()Tensors can be packaged and merged
 train_ds = TensorDataset(train_x,train_y)
-#希望模型不关注训练集数据顺序故用乱序
+#If you want the model to not pay attention to the order of the training set data, use the out-of-order order
 train_dl = DataLoader(train_ds,batch_size=batch,shuffle=True)
 test_ds = TensorDataset(test_x,test_y)
-#对测试集不需要用乱序避免工作量增加
+#There is no need to use out-of-order for the test set to avoid increased workload
 test_dl = DataLoader(test_ds,batch_size=batch)
 
-#创建模型
-#继承nn.Module这个类并自定义模型
+#Create a model
+#Inheritance nn. Module this class and customize the model
 class Model(nn.Module):
-    #定义初始化方法
+    #Define an initialization method
     def __init__(self):
-        #继承父类所有属性
+        #Inherits all properties of the parent class
         super().__init__()
-        #(初始化第一层 输入到隐藏层1)4个输入特征 32个输出特征
+        #(Initialize the first layer input to hidden layer 1) 4 input features 32 output features
         self.liner_1 = nn.Linear(21,32)
-        #(初始化第二层 隐藏层1到隐藏层2)32个输入特征 32个输出特征
+        #(Initialize the second layer hidden layer 1 to hidden layer 2) 32 input features 32 output features
         self.liner_2 = nn.Linear(32,32)
-        #(初始化第三层 隐藏层2到输出层)32个输入特征 3个输出特征
+        #(Initialize the third layer (hidden layer 2 to output layer) 32 input features 3 output features
         self.liner_3 = nn.Linear(32,16)
         self.liner_4 = nn.Linear(16,7)
-        #使用F不需要初始化激活层
+        #Using F does not require initializing the activation layer
 
-    #定义def forward会调用上述这些层来处理input
+    #Defining def forward calls these layers to handle inputs
     def forward(self,input):
-        #在第一层上对input进行调用 并激活
+        #Make a call to input on the first layer and activate
         x = F.relu(self.liner_1(input))
-        #在第二层上对input进行调用 并激活
+        #Make a call to input on the second layer and activate
         x = F.relu(self.liner_2(x))
-        #多分类任务并不激活，也有人写成如下所示，用softmax激活
+        #The multi-classification task is not activated, and some people write it as shown below, activated with softmax
         #x = F.softmax(self.liner_3(x))
-        #首先，nn.CrossEntropyLoss()函数的输入是一个未激活的输出
-        #其次，多项分类可以使用softmax函数进行激活
-        #然后，使用softmax()函数进行激活是将结果映射到一个0到1的概率分布上
-        #最后，如果不用softmax激活，输出最大的值依然是那个概率最高的值
+        #First, nn. The input to the CrossEntropyLoss() function is an inactive output
+        #Second, multinomial classifications can be activated using the softmax function
+        #Then, using the softmax() function for activation is to map the result onto a probability distribution from 0 to 1
+        #Finally, if you do not activate softmax, the maximum output value is still the value with the highest probability
         x = F.relu(self.liner_3(x))
         x = self.liner_4(x)
         return x
 
 model = Model()
-#损失函数
+#Loss function
 loss_fn = nn.CrossEntropyLoss()
 
 def accuracy(y_pred,y_true):
-    #torch.argmax将数字转换成真正的预测结果
+    #Torch.argmax converts numbers into real predictions
     y_pred = torch.argmax(y_pred,dim=1)
     acc = (y_pred == y_true).float().mean()
     return acc
 
-#便于随着训练的进行观察数值的变化
+#It is easy to observe the change in values as the training progresses
 train_loss=[]
 train_acc=[]
 test_loss=[]
 test_acc=[]
 
 def get_model():
-    #获得这个模型
+    #Get this model
     model = Model()
-    #优化函数 优化的是模型所有变量即model.parameters()
+    #Optimization function optimizes all variables of the model, i.e. model.parameters()
     opt = torch.optim.Adam(model.parameters(),lr=0.0001)
     return model,opt
 
@@ -96,13 +96,13 @@ for epoch in range(epochs):
     for x,y in train_dl:
         y_pred = model(x)
         loss = loss_fn(y_pred, y)
-        # 梯度置为0
+        # The gradient is set to 0
         optim.zero_grad()
-        # 反向传播求解梯度
+        # Backpropagation solves gradients
         loss.backward()
-        # 优化
+        # optimize
         optim.step()
-    # 不需要进行梯度计算
+    # No gradient calculations are required
     with torch.no_grad():
         epoch_accuracy = accuracy(model(train_x),train_y)
         epoch_loss = loss_fn(model(train_x), train_y).data
