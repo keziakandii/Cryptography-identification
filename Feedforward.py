59440e0f6dce68c6a08f3c5c625fc4dad0ff6234
Kezia <76508249+keziakandii@users.noreply.github.com>
2023-07-03 23:45:08 -0400

    Update Feedforward.py to english

--- Feedforward.py
+++ Feedforward.py
@@ -2,27 +2,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-data = pd.read_csv("3des_runs_1.csv") #读取csv格式的文件全部数据
+data = pd.read_csv("3des_runs_1.csv") #Read all data of the file in CSV format
 val_data = pd.read_csv('3des_runs_1.csv')
-data1 = np.mat(data) #数据矩阵化
+data1 = np.mat(data) #Data matrixization
 data2 = np.mat(val_data)
-pre_raw = 30 #需要预测值所在行数
+pre_raw = 30 #The number of rows where the predicted value is required
 # print (data1)
 def load_data_train():
-    data_train = data1[:,:] # 取出数据的第1行到26行中的第4列到47列作为训练数据
+    data_train = data1[:,:] # Columns 4 to 47 in rows 1 to 26 of the data are taken as training data
     data_val = data2[:,:]
     #print(data_train)
     return data_train,data_val
 
 def load_data_pre():
-    data_pre = data1[pre_raw, :].astype('float64') #第31行第4列到第46列为输入数据
-    data_mean = data_pre.mean() #求平均值
-    data_std = data_pre.std() #求标准差
-    data_pre = (data_pre - data_mean) / data_std #标准化
+    data_pre = data1[pre_raw, :].astype('float64') #Columns 4 to 46 of row 31 are input data
+    data_mean = data_pre.mean() #average
+    data_std = data_pre.std() #Find the standard deviation
+    data_pre = (data_pre - data_mean) / data_std #standardization
     return data_pre
 
 def load_data_real():
-    data_real = data1[pre_raw, ]  #取出第31行的第47列数据
+    data_real = data1[pre_raw, ]  #Take out column 31 of the data in row 47
     return data_real
 
 import numpy as np
@@ -32,21 +32,21 @@ from keras.layers.core import Dense, Activation
 
 
 def Train_Model(data_train,data_val):
-    #modelfile = './modelweight' #此位置保存训练模型过程中的权重
-    #y_mean_std = "./y_mean_std.txt" # 保存标准化过程中的数据，后边数据还原需要用到
+    #modelfile = './modelweight' #This location holds the weights during model training
+    #y_mean_std = "./y_mean_std.txt" # Save the data in the standardization process, which is required for later data restoration
     data_train = np.matrix(data_train).astype('float64')
     data_val = np.matrix(data_val).astype('float64')
-    data_mean = np.mean(data_train, axis=0)#对列求平均值
-    data_std = np.std(data_train, axis=0)#计算每一列的标准差
+    data_mean = np.mean(data_train, axis=0)#Average the columns
+    data_std = np.std(data_train, axis=0)#Calculates the standard deviation for each column
     # data_train = (data_train - data_mean) / data_std
     print(1)
-    x_train = data_train[:, 0:(data_train.shape[1] - 1)] #所有数据（除最后一列）作为输入x
-    y_train = data_train[:, data_train.shape[1] - 1] #所有数据的最后一列作为输出y
+    x_train = data_train[:, 0:(data_train.shape[1] - 1)] #All data (except the last column) as input x
+    y_train = data_train[:, data_train.shape[1] - 1] #The last column of all data is used as output y
     x_val = data_val[:, 0:(data_val.shape[1]-1)]
     y_val = data_val[:, data_val.shape[1]-1]
     #print(x_train)
     #print(y_train)
-    #模型训练
+    #Model training
     model = Sequential()
     model.add(Dense(x_train.shape[1], input_dim=x_train.shape[1], kernel_initializer="uniform"))
     model.add(Activation('relu'))
@@ -54,7 +54,7 @@ def Train_Model(data_train,data_val):
     model.add(Dense(1, input_dim=x_train.shape[1]))
     model.compile(loss='mean_squared_error', optimizer='adam',metrics=['accuracy'])
     history=model.fit(x_train, y_train, epochs=1000, batch_size=x_train.shape[0],validation_data=(x_val,y_val))
-    #model.save_weights(modelfile) #保存模型权重
+    #model.save_weights(modelfile) #Save the model weights
     y_mean = data_mean[:, data_train.shape[1] - 1]
     y_std = data_std[:, data_train.shape[1] - 1]
     acc = history.history['accuracy']
@@ -70,8 +70,8 @@ def Train_Model(data_train,data_val):
     plt.plot(epochs,val_loss,'b',label='validation loss')
     plt.legend()
     plt.show()
-    print("训练完毕")
+    print("Training is complete")
 
 Train_Model(data,val_data)
 
-#BP神经网络
+#BP neural networks
